bc87d50449b6e69bc9c20899fb834e9eae743f44
Jacob Scott <jscott12009@gmail.com>
2017-09-30 00:50:49 -0700

    Adding printing functionality and a couple more args

--- cypher_word_adding.py
+++ cypher_word_adding.py
@@ -7,23 +7,22 @@ together two numbers together and print the new word formed (with a little
 editing with a modulo for digits out of range on the cypher). Pass it n words
 and it will try the nChoose2 that are available and print them all out.
 '''
-from itertools import product, combinations_with_replacement
+from itertools import product, combinations_with_replacement, combinations
 from argparse import ArgumentParser
 
 
-CYPHER = [['a', 'b', 'c', 'd', 'e'],
-          ['f', 'g', 'h', 'i', 'j'],
-          ['k', 'l', 'm', 'n', 'o'],
-          ['p', 'q', 'r', 's', 't'],
-          ['u', 'v', 'w', 'x', 'yz']]
-'''
-CYPHER = [['a', 'b', 'c', 'd', 'e', 'f'],
-          ['g', 'h', 'i', 'j', 'k', 'l'],
-          ['m', 'n', 'o', 'p', 'q', 'r'],
-          ['s', 't', 'u', 'v', 'w', 'x'],
-          ['yz', ' ', '1', '2', '3', '4'],
-          ['5', '6', '7', '8', '9', '0']]
-'''
+CYPHER5 = [['a', 'b', 'c', 'd', 'e'],
+           ['f', 'g', 'h', 'i', 'j'],
+           ['k', 'l', 'm', 'n', 'o'],
+           ['p', 'q', 'r', 's', 't'],
+           ['u', 'v', 'w', 'x', 'yz']]
+CYPHER6 = [['a', 'b', 'c', 'd', 'e', 'f'],
+           ['g', 'h', 'i', 'j', 'k', 'l'],
+           ['m', 'n', 'o', 'p', 'q', 'r'],
+           ['s', 't', 'u', 'v', 'w', 'x'],
+           ['yz', ' ', '1', '2', '3', '4'],
+           ['5', '6', '7', '8', '9', '0']]
+CYPHER = CYPHER5
 
 def let_to_num(let):
     '''
@@ -103,7 +102,7 @@ def decypher_num(cyph_num):
     return word
 
 
-def calc_every_comb(*words, words_to_add):
+def calc_every_comb(*words, wta, keep_cyphers, replace):
     '''
     Calculates the resulting word from every combination of 2 words from the
     words given and then prints the data out. Words are tested with themselves.
@@ -111,12 +110,13 @@ def calc_every_comb(*words, words_to_add):
     Parameters:
     words - Args parameters of every word to use.
     '''
-    comb = combinations_with_replacement(words, int(words_to_add))
-    word_combs = []
-    cyph_nums_list = []
-    sum_nums_list = []
-    new_words = []
+    wta = int(wta)
+    if replace:
+        comb = combinations_with_replacement(words, wta)
+    else:
+        comb = combinations(words, wta)
 
+    word_combs, cyph_nums_list, sum_nums_list, new_words = [], [], [], []
     for word_comb in comb:
         word_combs.append(word_comb)
         # First turn words into numbers
@@ -127,29 +127,61 @@ def calc_every_comb(*words, words_to_add):
         new_words.append(decypher_num(sum_nums_list[-1]))
 
     # Print stuff!
+    # Constant lengths used for printing
     max_len_w = len(max(words, key=len))
     max_len_n = len(max(new_words, key=len))
     fdict = {'w': max_len_w if max_len_w > 8 else 8,
              'c': 2*max_len_w if 2*max_len_w > 10 else 10,
              'n': max_len_n if max_len_n > 10 else 10}
-    row = '|{:^{w}}|{:^{w}}|{:^{n}}|{:^{c}}|{:^{c}}|{:^{c}}|'
-    print('+' + '-' * (5 + fdict['n'] + 2*fdict['w'] + 3*fdict['c']) + '+')
-    print(row.format('Word 1', 'Word 2', 'New Word', 'Cypher 1', 'Cypher 2',
-                     'Sum', **fdict))
-    print('|{}+{}+{}+{}+{}+{}|'.format(*['-' * fdict['w']] * 2,
-                                       '-' * fdict['n'],
-                                       *['-' * fdict['c']] * 3))
+
+    # Templates for title, bar under title and for each word combo row
+    row = '|' + wta * '{:^{w}}|' + '{:^{n}}|' + \
+                keep_cyphers * (wta + 1) * '{:^{c}}|'
+    row_titles = ['Word ' + str(x + 1) for x in range(wta)] + ['New Word'] + \
+                 keep_cyphers * (['Cypher ' + str(x + 1)
+                                  for x in range(wta)] + ['Sum'])
+    row_bar = '|' + (wta + keep_cyphers * (wta + 1)) * '{}+' + '{}|'
+    row_ext = '+' + '-' * (keep_cyphers*((1 + fdict['c']) * (wta + 1)) + \
+                          wta + fdict['n'] + wta*fdict['w']) + '+'
+
+    print(row_ext)
+    print(row.format(*row_titles, **fdict))
+    print(row_bar.format(*['-' * fdict['w']] * wta, '-' * fdict['n'],
+                         *['-' * fdict['c']] * (wta + 1)))
     for n, word_comb in enumerate(word_combs):
         print(row.format(*word_comb, new_words[n], *cyph_nums_list[n],
                          sum_nums_list[n], **fdict))
-    print('+' + '-' * (5 + fdict['n'] + 2*fdict['w'] + 3*fdict['c']) + '+')
+    print(row_ext)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='Cypher words and return the word of' +
-                            'their sums. Give space-separated words to sum.')
+                            'their sums. Give space-separated words to sum. ' +
+                            'By default, will print the sum of 2 words at a ' +
+                            'time and print the cypher numbers.')
     parser.add_argument('-n', metavar='num', help='Number of words to sum')
+    parser.add_argument('-c', metavar='keep_cypher', help='Yes/True will ' +
+                        'print the cypher numbers. No/False will not.')
+    parser.add_argument('-r', metavar='duplicates', help='Yes/True will' +
+                        'allow duplicates for summing. No/False will not.')
     parser.add_argument('words', help='Words to sum', nargs='*')
     args = parser.parse_args()
 
-    calc_every_comb(*args.words, words_to_add=args.n or 2)
+    # Check to make sure -c flag is correct
+    if args.c is None or args.c.lower() in ['yes', 'y', 'true', 't']:
+        KEEP_CYPHERS = True
+    elif args.c.lower() in ['no', 'n', 'false', 'f']:
+        KEEP_CYPHERS = False
+    else:
+        raise Exception('Unknown keyword for -c flag: {}'.format(args.c))
+
+    # Check to make sure -r flag is correct
+    if args.r is None or args.r.lower() in ['yes', 'y', 'true', 't']:
+        REPLACE = True
+    elif args.c.lower() in ['no', 'n', 'false', 'f']:
+        REPLACE = False
+    else:
+        raise Exception('Unknown keyword for -r flag: {}'.format(args.c))
+
+    calc_every_comb(*args.words, wta=args.n or 2,
+                    keep_cyphers=KEEP_CYPHERS, replace=REPLACE)
